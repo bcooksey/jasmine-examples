9f8692d99b9f23f7b5a6d5db7fac34500ad8e998
Brian Cooksey <bac2k@hotmail.com>
2014-01-05 15:59:22 -0600

    switch up how a test is written to show off object comparison

--- spec/card-with-setup.js
+++ spec/card-with-setup.js
@@ -1,12 +1,13 @@
 describe('Credit Card', function() {
-   var card;
+   var card, cardData;
 
    beforeEach(function() {
-      card = new Card({
+      cardData = {
          'number': 123456789,
          'expirationDate': '1/1/2015',
          'cvc': 123
-      });
+      };
+      card = new Card(cardData);
 
       spyOn(Stripe.card, 'createToken');
    });
@@ -24,10 +25,6 @@ describe('Credit Card', function() {
    it('should create a credit in Stripe when a purchase is cancelled', function() {
       card.credit(50);
 
-      expect(Stripe.card.createToken).toHaveBeenCalledWith({
-         'number': 123456789,
-         'expirationDate': '1/1/2015',
-         'cvc': 123
-      });
+      expect(Stripe.card.createToken).toHaveBeenCalledWith(cardData);
    });
 });
