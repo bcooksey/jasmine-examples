d80ce1e1cfc1dc33778b52068995e9e7e19ced3b
Brian Cooksey <bac2k@hotmail.com>
2014-01-15 08:59:28 -0600

    Fix messed up test

--- spec/card.js
+++ spec/card.js
@@ -25,7 +25,7 @@ describe('Credit Card', function() {
    it('should create a credit in Stripe when a purchase is cancelled', function() {
       var card = new Card({
          'number': 123456789,
-         'expirationdate': '1/1/2015',
+         'expiration': '1/1/2015',
          'cvc': 123
       });
 
@@ -36,7 +36,7 @@ describe('Credit Card', function() {
 
       expect(Stripe.card.createToken).toHaveBeenCalledWith({
          'number': 123456789,
-         'expirationdate': '1/1/2015',
+         'expiration': '1/1/2015',
          'cvc': 123
       });
 
--- spec/converter.js
+++ spec/converter.js
@@ -1,14 +1,3 @@
 describe('Currency Converter', function() {
 
-   it('should properly convert Bitcoin to USD', function() {
-      expect(Converter.bitcoinToUsd(1)).toBe(918);
-   });
-
-   it('should properly convert Dogecoin to USD', function() {
-      expect(Converter.dogecoinToUsd(1)).toBe(0.00027);
-   });
-
-   it('should throw an exception on a negative amount', function() {
-      expect(Converter.bitcoinToUsd.bind(null, -1)).toThrow();
-   });
 });
